d9fad75029e9a1a200e5dd5cca0c264c94a946be
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-14 15:59:51 +0700

    Percobaan 3 penamaan variabel di java

---
+++ src/C_Variabel.java
@@ -0,0 +1,9 @@
+// percobaan untuk penamaan variabel
+
+public class C_Variabel {
+    public static void main(String[] args) {
+        double salary;
+        long _distance;
+        boolean $flag;
+    }
+}
