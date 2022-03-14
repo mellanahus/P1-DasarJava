5071d22e39d33228682fa804712eb1ccadbfb321
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-14 15:49:56 +0700

    Percobaan 2 menggunakan whitespace di java

--- src/B_Whitespace.java
+++ src/B_Whitespace.java
@@ -1,3 +1,1 @@
-// percobaan menggunakan whitespace(spasi dan enter) di java
-
 public class B_Whitespace { public static void main(String[] args) { System.out.println("Pemrograman Berorientasi Objek dengan Java"); } }
