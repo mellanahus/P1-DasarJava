ee199e03834d51b293393c641a7caf4999b631f4
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-14 15:21:37 +0700

    Initial projek pertemuan 1 dasar java

---
+++ src/A_BlokDanKomentar.java
@@ -0,0 +1,11 @@
+public class A_BlokDanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek dengan Java");
+        //contoh komentar 1 baris
+        /*contoh komentar lebih dari satu baris
+         */
+        /**
+         * JavaDoc--> baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
