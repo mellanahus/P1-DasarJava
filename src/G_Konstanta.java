5cde39b7084f9d9e63a2ae4b7f4bbd2178034fad
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-14 17:03:44 +0700

    Percobaan 7 mencetak nilai konstanta di java

---
+++ src/G_Konstanta.java
@@ -0,0 +1,11 @@
+// percobaan mencetak nilai konstanta
+
+public class G_Konstanta {
+    public static void main(String[] args) {
+
+        final double PHI = 3.14;
+        final double CM_PER_INCH = 2.54;
+        System.out.println("Nilai konstanta PHI = "+PHI);
+        System.out.println("Nilai konstanta CM_PER_INCH = "+ CM_PER_INCH);
+    }
+}
