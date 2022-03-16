1d07bf2a48306a34ec3664bf53dac551b1fb5878
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-17 06:13:37 +0700

    Percobaan 11 mencetak literal character di java dasar

---
+++ src/K_LiteralChar.java
@@ -0,0 +1,16 @@
+// percobaan mencetak literal character
+
+public class K_LiteralChar {
+    public static void main(String[] args) {
+
+        char tab = '\t';
+        char newline = '\n';
+        char singleQuote = '\'';
+        char doubleQuote = '\'';
+
+        System.out.println("Ini contoh" + tab + "literal tab");
+        System.out.println("Ini contoh" + newline + "literal newline");
+        System.out.println("" + singleQuote + "Ini contoh literal single quote" + singleQuote);
+        System.out.println("" + doubleQuote + "Ini contoh literal double quote");
+    }
+}
