bc02f73573d6b83cb404e036aa9d2646c23d7d8f
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-17 07:41:20 +0700

    Percobaan 13 untuk mencetak literal Sring di java dasar

---
+++ src/M_LiteralString.java
@@ -0,0 +1,9 @@
+// percobaan untuk mencetak literal string
+
+public class M_LiteralString {
+    public static void main(String[] args) {
+
+        String welcome = "Pemrograman Berorientasi Objek dengan Java";
+        System.out.println(welcome);
+    }
+}
