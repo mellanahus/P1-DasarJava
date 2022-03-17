4bd64351ccc00e93b3b6d49e260a174766469a56
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-17 07:45:10 +0700

    Percobaan 16 untuk contoh statement di java dasar

---
+++ src/P_Statement.java
@@ -0,0 +1,13 @@
+// percobaan untuk contoh statement
+
+public class P_Statement {
+    public static void main(String[] args) {
+
+        System.out.print("Semoga saya bisa belajar ");
+        System.out.println("Pemrograman Berorientasi Objek dengan Java");
+        int angka = 10;
+        char charA = 'A';
+        System.out.println(angka);
+        System.out.println(charA);
+    }
+}
