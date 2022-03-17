952b74f6a3b31a1953f60ce483b54b7d1166fe8f
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-17 08:16:47 +0700

    Percobaan 22 mengkonversi nilai masukan dari keyboard ke tipe tertentu menggunakan method bawaan kelas Scanner di java dasar

---
+++ src/V_InputTyperScannerB.java
@@ -0,0 +1,21 @@
+/* percobaan mengkonversi nilai masukan dari keyboard
+ke tipe tertentu menggunakan method bawaan kelas Scanner
+ */
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class V_InputTyperScannerB {
+    public static void main(String[] args) throws IOException{
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nama : ");
+        String name = input.nextLine();
+        System.out.print("Masukkan umur : ");
+        int age = input.nextInt();
+        System.out.print("Masukkan tinggi badan : ");
+        double height = input.nextDouble();
+        System.out.print("Sudah menikah (true/false) : ");
+        boolean married = input.nextBoolean();
+        System.out.println("Nama : "+name);
+    }
+}
