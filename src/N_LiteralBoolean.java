27bbb6641166e265b274c4f51fafdf5498fc4340
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-17 07:42:31 +0700

    Percobaan 14 untuk mencetak literal Boolean di java dasar

---
+++ src/N_LiteralBoolean.java
@@ -0,0 +1,11 @@
+// percobaan untuk mencetak literal boolean
+
+public class N_LiteralBoolean {
+    public static void main(String[] args) {
+
+        boolean isOpen = true;
+        boolean isClose = false;
+        System.out.println("contoh literal boolean kondisi "+isOpen);
+        System.out.println("contoh literal boolean kondisi "+isClose);
+    }
+}
