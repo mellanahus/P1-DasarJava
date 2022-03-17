e93b184bd89b0bf8cfeaa8a1406f168fa75cce2a
mellanahus <Mellanahus02SJ@gmail.com>
2022-03-17 08:33:31 +0700

    Percobaan 23 format keluaran menggunakan fungsi System.out.printf() di java dasar

---
+++ src/W_OutprintF.java
@@ -0,0 +1,21 @@
+// format keluaran output menggunakan fungsi System.out.printf()
+
+public class W_OutprintF {
+    public static void main(String[] args) {
+        String nama = "Anton";
+        int umur = 21;
+        float tinggiBadan = 170.4f;
+        double beratBadan = 71.78;
+        boolean kawin = true;
+        char jenisKelamin = 'P';
+        int oktal12 = 014;
+        System.out.printf("Nama : %s\n",nama);
+        System.out.printf("Umur : %d\n",umur);
+        System.out.printf("Tinggi Badan : %f\n",tinggiBadan);
+        System.out.printf("Berat Badan : %1f\n",beratBadan);
+        System.out.printf("Sudah Menikah : %b\n", kawin);
+        System.out.printf("Jenis Kelamin : %c\n",jenisKelamin);
+        System.out.printf("Bilangan Oktal 12 : %o\n", oktal12);
+
+    }
+}
